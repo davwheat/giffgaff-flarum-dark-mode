e4bf572ba5be41daee213cefb505f8a9cf51cb5f
David Wheatley <davidwheatley03@gmail.com>
2019-10-27 18:15:28 +0100

    Update giffgaff-flarum-dark-mode.user.js

--- giffgaff-flarum-dark-mode.user.js
+++ giffgaff-flarum-dark-mode.user.js
@@ -3,7 +3,7 @@
 // @description    Adds a dark mode to giffgaff's community forum.
 // @author         David Wheatley <davidwheatley03@gmail.com> (https://github.com/davwheat/giffgaff-flarum-dark-mode)
 // @namespace      https://github.com/davwheat/giffgaff-flarum-dark-mode
-// @version        1.1
+// @version        1.2
 // @icon           https://github.com/davwheat/giffgaff-flarum-better-post-dates/blob/master/icon.png?raw=true
 // @match          *://community.giffgaff.com/*
 // @grant          none
